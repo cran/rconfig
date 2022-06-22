b0af47028f8b3a3a2166006ccedb493b40b4e8bc
Peter Solymos <peter@analythium.io>
2022-06-22 16:00:02 +0000

    version 0.1.3

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,8 @@
+# Version 0.1.3
+
+* Empty flag is invalid and throws an error (#3).
+* Allow verb arguments for sub-commands with a `command()` method to access these from within the scripts (#4).
+
 # Version 0.1.2
 
 * Added `value()` method (#2).
---
+++ inst/examples/commands.R
@@ -0,0 +1,27 @@
+#!/usr/bin/env Rscript
+CONFIG <- rconfig::rconfig()
+
+model <- function() {
+    message("Model ...")
+    # your logic comes here
+}
+
+pred <- function() {
+    message("Predict ...")
+    # your logic comes here
+}
+
+main <- function() {
+    cmd <- rconfig::command(CONFIG)
+    if (length(cmd) == 0L)
+        stop("Specify a command.", call. = FALSE)
+    if (cmd[1L] == "model") {
+        model()
+    } else if (cmd[1L] == "predict") {
+        pred()
+    } else {
+        stop("Command ", cmd, " not found.", call. = FALSE)
+    }
+}
+
+main()
